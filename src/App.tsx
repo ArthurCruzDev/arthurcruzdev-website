579663f426c28330f42ea0779ce2ea7990dc842d
DevArthurCruz <developer.arthur.cruz@gmail.com>
2022-08-28 10:53:36 -0300

    FEAT[ReactRouter]: Adding ReactRouter to the project and cleaning some create-react-app files and code.

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,12 @@
-import React, { useContext } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import { useTranslation } from "react-i18next";
-import AppContext from "./contexts/AppContext/AppContext";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Homepage from "./pages/homepage";
 
 function App() {
-  const { t } = useTranslation();
-  const appState = useContext(AppContext);
-
   return (
-    <div className={`App ${appState.theme}`}>
-      <h1 className="text-3xl font-bold dark:underline">
-        Hello world!
-      </h1>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{t("under.construction.title")}</p>
-        <p
-          onClick={() =>
-            appState.lang === "pt-BR"
-              ? appState.setLang("en-US")
-              : appState.setLang("pt-BR")
-          }
-        >
-          CLICK ME TO CHANGE LANGUAGE
-        </p>
-        <p
-          onClick={() =>
-            appState.theme === "dark" ? appState.setTheme("light") : appState.setTheme("dark")
-          }
-        >
-          CLICK ME TO CHANGE THEME
-        </p>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+    </Routes>
   );
 }
 
