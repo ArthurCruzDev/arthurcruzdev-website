8a23db5de9b5485cda2ac99bdfe5d4441229de59
DevArthurCruz <developer.arthur.cruz@gmail.com>
2022-12-06 22:42:57 -0300

    FEAT[Footer]: Improving footer
    - Added Github, LinkedIn and E-mail links with icons
    - Added hover animations and color change to icons
    - Adjusted footer height
    
    developer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,9 +1,34 @@
+import { VscGithubAlt } from "react-icons/vsc";
+import { SlSocialLinkedin } from "react-icons/sl";
+import { TfiEmail } from "react-icons/tfi";
+import styles from "./index.module.scss";
 export const Footer = () => {
   return (
-    <div className="bg-background w-100 h-60 mt-auto flex flex-col p-3">
-      <h6 className="text-center mt-auto text-xs font-semibold">
+    <div className="bg-background w-100 h-28 mt-auto flex flex-col p-3 justify-end items-center">
+      <div className="flex flex-row flex-nowrap justify-between w-36 h-8 ">
+        <a
+          href="https://github.com/ArthurCruzDev"
+          target={"_blank"}
+          rel="noreferrer"
+          className="w-8"
+        >
+          <VscGithubAlt className={styles.icon}></VscGithubAlt>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/arthur-rodrigues-cruz/"
+          target={"_blank"}
+          rel="noreferrer"
+          className="w-8"
+        >
+          <SlSocialLinkedin className={styles.icon}></SlSocialLinkedin>
+        </a>
+        <a href="mailto:developer.arthur.cruz@gmail.com" className="w-8">
+          <TfiEmail className={styles.icon}></TfiEmail>
+        </a>
+      </div>
+      <span className="text-center mt-4 text-xs font-semibold">
         Copyright&#169; Arthur Rodrigues Cruz 2022
-      </h6>
+      </span>
     </div>
   );
 };
